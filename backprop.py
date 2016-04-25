1ccbf2b1d73e6fcecb73f355cf3f9951491c5ddd
Nancy Wong <nancypwong@gmail.com>
2016-04-25 18:59:11 +1200

    Add stubbed functions, general network flow

--- backprop.py
+++ backprop.py
@@ -1,4 +1,14 @@
 """Implementation of backpropgating neural network."""
+import random
+
+
+### Misc Helper Functions
+def small_rand():
+    """Returns a small random value for initializing weights."""
+    init_rand = 0.3 # can adjust this value
+    return random.uniform(-init_rand, init_rand)
+
+
 class Neuron:
     """Units for neural network.
     Can be an input, hidden, or output neuron."""
@@ -9,8 +19,18 @@ class Neuron:
         self.post_neighbors = {} # map output Neurons -> weights
 
 
-    def update_weight(self, neuron, weight):
-        self.pre_neighbors[neuron] = weight
+    def update_weight(self, isForward, neuron, weight):
+        if isForward:
+            # forward propagation weight change
+            self.pre_neighbors[neuron] = weight
+        else:
+            # backward propagation weight change
+            self.post_neighbors[neuron] = weight
+
+
+    def activate(self, inputs):
+        """Sinoid activation function for a given unit."""
+        pass
 
 
     def __repr__(self):
@@ -33,8 +53,9 @@ class BackPropagationNeuralNetwork:
         self.MOMENTUM_CONSTANT = params_list[4]
         self.ERROR_CRITERION = params_list[5]
 
-        # Set target values
-        self.targets = targets_list
+        # Set input and values
+        self.input_patterns = inputs_list
+        self.target_values = targets_list
 
         # Initialize units
         self.input_units = []
@@ -70,43 +91,141 @@ class BackPropagationNeuralNetwork:
 
         self.output_units = output
 
-        ## Set neighbors and weights
+        ## Set neighbors and weights with small random values
         # Connect each input unit with each hidden unit
         for i in self.input_units:
             for h in self.hidden_units:
-                i.post_neighbors[h] = 0 # TODO initialize with small random value
-                h.pre_neighbors[i] = 0 # TODO initialize with small random value
+                i.post_neighbors[h] = small_rand()
+                h.pre_neighbors[i] = small_rand()
 
         # Connect each hidden unit with each output unit
         for h in self.hidden_units:
             for o in self.output_units:
-                h.post_neighbors[o] = 0 # TODO initialize weights
-                o.pre_neighbors[h] = 0 # TODO initialize weights
+                h.post_neighbors[o] = small_rand()
+                o.pre_neighbors[h] = small_rand()
 
 
-    def activate(self, val):
-        """Activation function for a given unit."""
+    def calculate_output_error(self, output, target):
+        """Calculate error between output and target values."""
+        diff_sum_squared = 0
+# TODO
+        """ for each output:
+        diff = target - output
+        diff_sum_squred += (diff * diff)"""
+
+        pattern_err = 0.5 * diff_sum_squared
+
+        return pattern_err
         pass
 
 
-    def error(self, output, target):
-        """Calculate error between output and target values."""
+    def calculate_hidden_error(self, hidden, target):
+        pass
+
+
+    def calculate_population_error(self, weight_changes):
+        """Calculate population error."""
+        # TODO
+        sum_err = 0
+        """ for each epoch (list of patterns):
+            sum_err += error(output, target)"""
+
+        n = self.num_output_units # number of output units
+        k = len(self.input_patterns) # number of patterns
+
+        pop_err = sum_err / n*k
+        return pop_err
+        pass
+
+
+    def consolidate_err(self, batch_err, net_err):
+        """Add net error mapping to batch error mapping.
+        Return new batch error."""
+        # TODO: add net to batch err
+
+        return batch_err
         pass
 
 
     def train(self):
         """Train neural network"""
-        # Forward propagation
 
-        # Backward propagation
+        num_epochs = 0
+        population_err = float('inf')  # set to max
+        weight_changes = {} # map (neuron1, neuron2) -> change value
 
-        # Update weights
+        # Batch learning
+        while self.ERROR_CRITERION < population_err:
+            num_epochs += 1
+            if num_epochs % 100 == 0:
+                print num_epochs
+                print population_err
+
+            batch_err = {} # error per epoch, map (neuron1, neuron2) -> err val
+
+            for pattern in self.input_patterns:
+                self.set_input_units(pattern)
+
+                # Forward propagation
+                self.feed_forward()
+
+                # Backward propagation
+                net_err, weight_changes = self.back_propagate()
+
+                # Update weights using Delta Rule
+                self.update_weights(weight_changes)
+                batch_err = self.consolidate_err(batch_err, net_err)
+
+
+            # TODO: population_err += batch_err
+            population_err = self.calculate_population_error(weight_changes)
 
         pass
 
 
+    def feed_forward(self):
+        """Propagate forward and calculate output activations for each neuron
+        in the network. Set output units to output activation values."""
+
+        # TODO forward calculations
+        self.output_units = [] # TODO set with calculated values
+
+        pass
+
+
+    def back_propagate(self):
+        """Propagate backward, using target values to calculate error for
+        weight changes for all output and hidden neurons.
+        Return a map of (neuron1, neuron2) -> delta value."""
+        net_err = {}  # maps (neuron1, neuron2) -> error value
+        weight_changes = {}
+
+        # TODO backward calculations
+        return net_err, weight_changes
+
+
+    def set_input_units(self, pattern):
+        """Load input units with pattern values."""
+        for i, n in enumerate(self.input_units):
+            n.value = pattern[i]
+
+
+    def update_weights(self, weight_changes):
+        # TODO: for each entry in weight_changes, update_weight
+        pass
+
+
+    def update_weight(self, in_unit, out_unit, target):
+        o = in_unit.post_neighbors[out_unit] # output weight, aka significance
+        diff = target # - output?
+
+        delta = self.LEARNING_CONSTANT * o * diff
+        in_unit.post_neighbors[out_unit] += delta # update weight
+
+
     def predict(self, input_pattern):
-        """Given input pattern, predict output"""
+        """Given input pattern, predict output by running it through the neural
+        net."""
         pass
 
 
